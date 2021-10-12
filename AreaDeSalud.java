c690f66c62f43f2d287971835ae79bd23d2ceb32
Olman Alonso Acuña Quesada <61434202+OlmanAQ@users.noreply.github.com>
2021-10-11 21:06:05 -0600

    Cambio de nombre clase

--- AreaDeSalud.java
+++ AreaDeSalud.java
@@ -182,7 +182,6 @@ public Vector<Personas> getPacientes() {
 
   public String toString()
 	{
-    
 		return(String.format("Nombre: %s\nDirector: %s\nDirección: %s\nÁrea de Atracción: %s\n", 
 			this.nombre,this.personaDirectora,this.direccion,this.areaDeAtraccion));
 
--- pruebasCOVID19.java
+++ pruebasCOVID19.java
@@ -18,7 +18,7 @@ public class pruebasCOVID19 {
    */
   private String nombredelPersonal;
 
-  public tiposPrueba m_tipo;
+  public TiposPrueba m_tipo;
   
   //
   // Constructors
@@ -88,7 +88,7 @@ public class pruebasCOVID19 {
    * Set the value of m_tipo
    * @param newVar the new value of m_tipo
    */
-  public void setTipo (tiposPrueba newVar) {
+  public void setTipo (TiposPrueba newVar) {
     m_tipo = newVar;
   }
 
@@ -96,7 +96,7 @@ public class pruebasCOVID19 {
    * Get the value of m_tipo
    * @return the value of m_tipo
    */
-  public tiposPrueba getTipo () {
+  public TiposPrueba getTipo () {
     return m_tipo;
   }
 
